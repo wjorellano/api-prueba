134973d1147c41272678a4a761f6971011551ca8
willman orellano <wilmanjunior2001@gmail.com>
2022-08-08 00:06:06 -0500

    configuracion con graphql

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  if Rails.env.development? || Rails.env.staging?
+  if Rails.env.development? || Rails.env.production? 
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
   post "/graphql", to: "graphql#execute"
   mount ActionCable.server, at: '/cable'
 
-  root 'static_pages#home'
+  # root 'static_pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
